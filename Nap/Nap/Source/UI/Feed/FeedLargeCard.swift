88adf030f5270ddf20f3c32ab3967e15d8f76fdb
YunHak Lee <lyh990408@gmail.com>
2024-07-24 16:23:29 +0900

    [Add] #6 - FeedLargeCard 파일 추가

---
+++ Nap/Nap/Source/UI/Feed/FeedLargeCard.swift
@@ -0,0 +1,18 @@
+//
+//  FeedLargeCard.swift
+//  Nap
+//
+//  Created by YunhakLee on 7/24/24.
+//
+
+import SwiftUI
+
+struct FeedLargeCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedLargeCard()
+}
