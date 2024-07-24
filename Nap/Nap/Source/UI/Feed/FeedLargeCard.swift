12d09e10ed38b39a92f46e31f39adbd66b42ea96
YunHak Lee <lyh990408@gmail.com>
2024-07-24 18:27:34 +0900

    [Add] #6 - FeedLargeCard View 이용자 프로필 부분까지 추가

--- Nap/Nap/Source/UI/Feed/FeedLargeCard.swift
+++ Nap/Nap/Source/UI/Feed/FeedLargeCard.swift
@@ -8,11 +8,48 @@
 import SwiftUI
 
 struct FeedLargeCard: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Image(.feedImage1)
+            .resizable()
+            .scaledToFill()
+            .frame(width: 300, height: 400)
+            .clipShape(RoundedRectangle(cornerRadius: 20.0))
+            .overlay(alignment: .topLeading) {
+                UserProfile
+                    .padding(16)
+            }
+            
+    }
+}
+
+extension FeedLargeCard {
+    var UserProfile: some View {
+        HStack {
+            Image(.feedImage6)
+                .resizable()
+                .scaledToFill()
+                .clipShape(Circle())
+                .frame(width: 32, height: 32)
+            VStack(alignment: .leading, spacing: 1) {
+                Text("hash.jung")
+                    .font(.system(size: 14, weight: .semibold))
+                    .foregroundStyle(.white)
+                Text("2시간 전")
+                    .font(.system(size: 10, weight: .regular))
+                    .foregroundStyle(.white)
+            }
+        }
+        .padding(6)
+        .padding(.trailing, 10)
+        .background {
+            BackgroundBlur(radius: 6)
+                .clipShape(Capsule())
+        }
     }
 }
 
 #Preview {
     FeedLargeCard()
 }
+
