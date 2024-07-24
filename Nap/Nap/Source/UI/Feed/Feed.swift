0d99e3b9a26f6986d53a7a61c8ff4d4a7c94c158
YunHak Lee <lyh990408@gmail.com>
2024-07-25 01:10:34 +0900

    [Design] #6 - 피드뷰 대강 구현 완료

--- Nap/Nap/Source/UI/Feed/Feed.swift
+++ Nap/Nap/Source/UI/Feed/Feed.swift
@@ -11,7 +11,7 @@ struct Feed: View {
     @State var isLargeCard: Bool = true
     
     var columns: [GridItem] {
-        isLargeCard ? [GridItem()] : [GridItem(), GridItem()]
+        isLargeCard ? [GridItem(.flexible())] : [GridItem(.flexible(), spacing: 12), GridItem(.flexible(), spacing: 12)]
     }
     
     var spacing: CGFloat {
@@ -19,24 +19,103 @@ struct Feed: View {
     }
     
     var body: some View {
-        GeometryReader { proxy in
-            let cardWidth = isLargeCard ? proxy.size.width :
-                                         (proxy.size.width-20)/2
-            ScrollView {
-                LazyVGrid(columns: columns, spacing: spacing) {
-                    ForEach(0..<10) { _ in
-                        FeedLargeCard(showInformation: $isLargeCard)
-                            .frame(width: cardWidth, height: cardWidth/3*4)
+        NavigationStack {
+            ZStack(alignment: .bottomTrailing) {
+                ScrollView {
+                    LazyVGrid(columns: columns, spacing: spacing) {
+                        ForEach(0..<10) { _ in
+                            FeedLargeCard(showInformation: $isLargeCard)
+                        }
                     }
                 }
+                .scrollIndicators(.never)
+                .contentMargins(12, for: .scrollContent)
+                .padding(.top, 18)
+                .padding(.horizontal, 4)
+                // 기본 padding 16으로 줄이는건 어떤지
+                
+                FloatingButton
+                    .padding(.trailing, 28)
             }
-            .scrollIndicators(.never)
+            .background {
+                Image(.feedBackground)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .ignoresSafeArea()
+            }
+            
+            .navigationTitle("")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Header
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    FriendCount
+                }
+            }
         }
-        .padding(.horizontal, 20)
+    }
+}
+
+extension Feed {
+    var Header: some View {
+        Text("Feed")
+            .foregroundStyle(.white)
+            .font(.system(size: 24, weight: .bold))
+    }
+    
+    var FriendCount: some View {
+        HStack(spacing: 0) {
+            Image(systemName: "person.2.fill")
+                .foregroundStyle(.white)
+                .font(.system(size: 14, weight: .regular))
+            Spacer().frame(width: 4)
+            Text("친구")
+                .foregroundStyle(.white)
+                .font(.system(size: 14, weight: .bold))
+            // figma 폰트에서는 한글은 bold - nonbold 두가지 밖에 안되는듯 ?
+            Spacer().frame(width: 12)
+            Text("4")
+                .foregroundStyle(.white)
+                .font(.system(size: 14, weight: .medium))
+            Spacer().frame(width: 2)
+            Text("/")
+                .foregroundStyle(.white.opacity(0.4))
+                .font(.system(size: 11, weight: .bold))
+            Spacer().frame(width: 2)
+            Text("6")
+                .foregroundStyle(.white.opacity(0.4))
+                .font(.system(size: 14, weight: .medium))
+        }
+        .padding(.horizontal, 12)
+        .padding(.vertical, 8)
         .background {
-            Image(.feedBackground)
-                .ignoresSafeArea()
+            Capsule()
+                .foregroundStyle(.white.opacity(0.1))
         }
+    }
+    
+    var FloatingButton: some View {
+        Image(systemName: isLargeCard ? "arrow.up.right.and.arrow.down.left" :
+                "arrow.down.backward.and.arrow.up.forward")
+        .resizable()
+        .scaledToFit()
+        .foregroundStyle(.white)
+        .frame(width: 27, height: 27)
+        .padding(20)
+        .background {
+            Circle()
+                .foregroundStyle(.ultraThinMaterial)
+                .overlay {
+                    Circle()
+                        .stroke(.white.opacity(0.2), lineWidth: 2.0)
+                }
+        }
+        .onTapGesture {
+            isLargeCard.toggle()
+        }
+        .animation(.easeInOut(duration: 0.2), value: isLargeCard)
     }
 }
 
