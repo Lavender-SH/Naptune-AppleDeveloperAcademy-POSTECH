05a2605cb83977fefe024bb9e378c0a02f15cab3
interstellaar <https://github.com/>
2024-07-25 10:47:46 +0900

    [Feat] #2 - FB 연동,애플로그인

--- Nap/Nap/Source/App/NapApp.swift
+++ Nap/Nap/Source/App/NapApp.swift
@@ -6,12 +6,30 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct NapApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    @State private var loginComplete = UserDefaults.standard.bool(forKey: "loginComplete")
+
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        Onboarding(loginComplete: $loginComplete)
+      }
     }
+  }
 }
