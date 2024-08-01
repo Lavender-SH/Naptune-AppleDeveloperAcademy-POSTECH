b7bcb184ba3652f3c6b69d63a020c79fe7d05218
YunHak Lee <lyh990408@gmail.com>
2024-08-01 18:16:48 +0900

    [Design] #24 - 파일 정리

--- Nap/Nap/Source/UI/ContentView.swift
+++ Nap/Nap/Source/UI/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
     
     var body: some View {
         if isOnboarding {
-            NagiOnboarding(isOnboarding: $isOnboarding)
+            Onboarding(isOnboarding: $isOnboarding)
         } else {
             Main
         }
