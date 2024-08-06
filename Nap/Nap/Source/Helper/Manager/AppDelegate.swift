e45de3eec5b8266e7f98b23649691a9b1c4efb03
interstellaar <https://github.com/>
2024-08-06 20:41:58 +0900

    #50 타겟,앱델리게이트 임시저장

---
+++ Nap/Nap/Source/Helper/Manager/AppDelegate.swift
@@ -0,0 +1,51 @@
+//
+//  AppDelegate.swift
+//  Nap
+//
+//  Created by Jeho Ahn on 8/6/24.
+//
+
+import UIKit
+import Firebasecore
+import FirebaseAuth
+import FirebaseMessaging
+import UserNotifications
+
+class AppDelegate: NSObject, UIApplicationDelegate, MessagingDelegate, UNUserNotificationCenterDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+            if let error = error {
+                print("Error requesting notification authorization: \(error)")
+            }
+        }
+        
+        application.registerForRemoteNotifications()
+        Messaging.messaging().delegate = self
+        
+        // Firebase Authentication 설정
+        if Auth.auth().currentUser == nil {
+            Auth.auth().signInAnonymously { (authResult, error) in
+                if let error = error {
+                    print("Firebase anonymous authentication failed: \(error.localizedDescription)")
+                } else {
+                    print("Firebase anonymous authentication succeeded")
+                }
+            }
+        }
+        
+        return true
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        if let fcmToken = fcmToken {
+            print("FCM Token: \(fcmToken)")
+        }
+    }
+}
